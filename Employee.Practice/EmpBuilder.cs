a39e7ba407889b68f5c8ff04e71fef1b7fe0fce3
HOWTOPASSAUSERNAME <130306110+HOWTOPASSAUSERNAME@users.noreply.github.com>
2023-05-01 22:24:57 +0800

    v1

---
+++ Employee.Practice/EmpBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Practice
+{
+    public class EmpBuilder
+    {
+
+        private List<Employee> _employees = new List<Employee>();
+        public static EmpBuilder Create()
+        {
+            return new EmpBuilder();
+        } 
+
+        public EmpBuilder SetName(string lastName,string firstName)
+        {
+            var employee = new Employee() {LastName =lastName,FirstName=firstName };
+            _employees.Add(employee);
+            return this;
+        }
+
+        public EmpBuilder SetEmail(string email)
+        {
+            var employee = new Employee() { Email = email };
+            _employees.Add(employee);
+            return this;
+        }
+
+        public EmpBuilder Address(string country, int postalCode, string city, string address) {
+
+            var employee = new Employee() 
+            { 
+                Country = country
+                ,PostalCode=postalCode
+                ,City = city
+                ,Address = address
+            };
+            _employees.Add(employee);
+            return this;
+        }
+
+        public EmpBuilder SetGender(bool gender)
+        {
+            var employee = new Employee() { Gender = gender };
+            _employees.Add(employee);
+            return this;
+
+        }
+        public  Employee[]Build()
+        {
+            return _employees.ToArray();
+
+        }
+
+    }
+}
---
+++ Employee.Practice/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] kai = EmpBuilder.Create()
+                .SetName("Kai", "Lin")
+                .SetEmail("solarsido@gmail.com")
+                .Address("台灣",700,"台南市","中華西路一段111號")
+                .SetGender(true)
+                .Build();
+
+            
+        }
+    }
+
+    public class Employee
+    {
+        public string FirstName {
+            get
+            {
+                return FirstName;
+            }
+                
+            set
+            {
+                if (string.IsNullOrEmpty(value)) { throw new ArgumentNullException("Null,請重新輸入"); }
+                FirstName = value;
+            }
+                 }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int PostalCode { get; set; }
+        public string Country { get; set; }
+
+        public bool  Gender { get; set; }
+
+    }
+}
