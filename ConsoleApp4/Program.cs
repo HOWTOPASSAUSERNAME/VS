189c3a2236142be0109d9c970394c3f04274d1f3
HOWTOPASSAUSERNAME <130306110+HOWTOPASSAUSERNAME@users.noreply.github.com>
2023-05-03 16:49:50 +0800

    updatetostring

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,58 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp4
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Func<int, int, string> right = (row, num) => { return new string('*', row).PadLeft(num, ' '); };
 
 
-            Func<int, int, StringBuilder, StringBuilder> right = (row, num,sb) =>
+            Func<int, int, string> left = (row, num) => { return new string('*', row); };
+
+            Func<int, int, string> middle = (row, num) =>
             {
-                return sb.AppendLine(new string('*', row).PadLeft(num, ' '));
-
-            };
-
-
-            Func<int, int, StringBuilder, StringBuilder> left  = (row, num, sb) =>
-            {
-                return sb.AppendLine(new string('*', row));
-
-            };
-
-            Func<int, int, StringBuilder, StringBuilder> middle = (row, num, sb) =>
-            {
-                return sb.AppendLine(new string(' ', num-row)+new string('*', 2*row-1));
-
+                return new string(' ', num - row) + new string('*', 2 * row - 1);
             };
 
 
             Console.WriteLine(new Graphic().Add(5, right));
             Console.WriteLine(new Graphic().Add(5, left));
-            Console.WriteLine(new Graphic().Add(5,middle));
-
+            Console.WriteLine(new Graphic().Add(5, middle));
         }
     }
 
-    public class Graphic { 
-    
-        public string Add(int num , Func<int, int, StringBuilder, StringBuilder> func)
+    public class Graphic
+    {
+        public string Add(int num, Func<int, int, string> func)
         {
+            var result = string.Empty;
+            for (var row = 1; row <= num; row++) result += func(row, num) + "\r\n";
 
-            StringBuilder sb = new StringBuilder();
-            for (int row = 1; row <= num; row++)
-            {
-                 func(row,num,sb);
-            }
-            return sb.ToString();
-            
-
+            return result;
         }
-    
     }
 }
